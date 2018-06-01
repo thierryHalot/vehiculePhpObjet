086d98f4720b56785aed51cc9841da250b64371c
Thierry <thierry.halot@laposte.net>
2018-06-01 09:44:32 +0200

    rajout d'une class absraite Vehicule

---
+++ Vehicule.php
@@ -0,0 +1,13 @@
+<?php
+abstract class Vehicule implements Interface_Deplacement
+//tous mes objects qui hérite de cette classe avec le mot clé extend auront acces à ces propriétés
+
+{
+    public $resitance;
+    public $impact;
+    public $poid;
+    public $vitesse;
+    public $reservoir;
+
+
+}
